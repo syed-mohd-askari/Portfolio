606e71f632c5d10ce37eb356731636a4ee662305
Syed Mohd Askari <mohdaskari48@gmail.com>
2024-09-21 01:18:51 +0530

    Testimonials Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,43 @@ const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 sidebarBtn.addEventListener("click" , function(){
     elementToggleFunc(sidebar);
 })
+
+// testimonials variables
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+// modal variable
+const modalImg = document.querySelector("[data-modal-img]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
+const modalDate = document.querySelector("[data-modal-date]");
+
+// modal toggle function
+const testimonialsModalFunc = function () {
+    modalContainer.classList.toggle("active");
+    overlay.classList.toggle("active");
+  }
+
+
+// add click event to all modal items
+for (let i = 0; i < testimonialsItem.length; i++) {
+
+    testimonialsItem[i].addEventListener("click", function () {
+  
+      modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+      modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+      modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
+      modalDate.innerHTML = this.querySelector("[data-testimonial-date]").innerHTML;
+      modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+  
+      testimonialsModalFunc();
+  
+    });
+  
+}
+
+// add click event to modal close button
+modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+overlay.addEventListener("click", testimonialsModalFunc);
