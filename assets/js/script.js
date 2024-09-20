7fc296f3a46efe2fd441457dbcdde3ec2855dd27
Syed Mohd Askari <mohdaskari48@gmail.com>
2024-09-20 18:58:36 +0530

    Toggle active sidebar

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const elementToggleFunc = function(elem){
+    elem.classList.toggle("active");
+}
+
+
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+sidebarBtn.addEventListener("click" , function(){
+    elementToggleFunc(sidebar);
+})
